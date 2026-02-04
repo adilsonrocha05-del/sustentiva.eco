882006b71f2ebd4a85d3213b937966463f750324
Guilherme FGR <guifgr@outlook.com>
2026-02-04 20:46:36 -0300

    fix: Restore script.js to fix mobile menu

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('nav ul');
+  
+    toggle.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  });
+  
