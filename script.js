a3a1c8d556aec10e222ef78f265f1d789678f014
Guilherme FGR <guifgr@outlook.com>
2026-02-04 20:36:55 -0300

    feat: Restore 1.jpg and set as hero background

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('nav ul');
-  
-    toggle.addEventListener('click', function () {
-      menu.classList.toggle('active');
-    });
-  });
-  
